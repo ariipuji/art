6e3dd5a54e7424a744a9290e2f1f20a70f359466
ariipuji <42854143+ariipuji@users.noreply.github.com>
2018-09-07 03:04:01 +0700

    Update and rename calljs to calljs.js

--- calljs.js
+++ calljs.js
@@ -1,4 +1,4 @@
-<script src='https://raw.githubusercontent.com/ariipuji/art/master/scriptjs' type='text/javascript'/>
+<script src='https://raw.githubusercontent.com/ariipuji/art/master/scriptjs.js' type='text/javascript'/>
 <script type='text/javascript'>
 protected_links = &quot;namadomainsobat.com,blogspot.com,blogger.com,linkedin.com,facebook.com,twitter.com,google.com,feedburner.com&quot;;
 auto_safelink();
